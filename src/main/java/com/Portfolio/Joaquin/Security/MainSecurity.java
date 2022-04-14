43521870e9de89297b93dc128a4c6eec46ae6dea
Joaquin Cornelatti <Darkjoa.jc@gmail.com>
2022-04-14 11:53:10 -0300

    changes2.0.9

--- src/main/java/com/Portfolio/Joaquin/Security/MainSecurity.java
+++ src/main/java/com/Portfolio/Joaquin/Security/MainSecurity.java
@@ -56,4 +56,17 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers().permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 }
