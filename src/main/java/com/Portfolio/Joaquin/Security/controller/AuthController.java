72ab084ce05d8b3fb9a55aca599ad791e4e4bf8c
Joaquin Cornelatti <Darkjoa.jc@gmail.com>
2022-04-14 01:31:03 -0300

    changes2

--- src/main/java/com/Portfolio/Joaquin/Security/controller/AuthController.java
+++ src/main/java/com/Portfolio/Joaquin/Security/controller/AuthController.java
@@ -32,7 +32,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://joaquincornelatti-f320d.web.app")
 public class AuthController {
    @Autowired
     PasswordEncoder passwordEncoder;
